88a68e1346bf9c2fc2af8f0ae983d48fe95933d7
Casweeney Ojukwu <casweeno2000@gmail.com>
2021-05-31 10:54:44 +0100

    Loan calculation and error trigger

--- app.js
+++ app.js
@@ -13,6 +13,48 @@ function calculateResults(e){
     const totalInterest = document.querySelector('#total-interest');
 
     const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest)/(x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        showError('Please check your numbers');
+    }
 
     e.preventDefault();
+}
+
+// Show Error
+function showError(errorMsg){
+    //Create a div
+    const errorDiv = document.createElement('div');
+
+    //Get DOM elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    //Add Bootstrap Class
+    errorDiv.className = 'alert alert-danger';
+
+    //Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(errorMsg));
+
+    //Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    //Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+
+}
+
+//Clear error
+function clearError(){
+    document.querySelector('.alert').remove();
 }
